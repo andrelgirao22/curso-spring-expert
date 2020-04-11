39c5968d9f4a41f729c908d965bd758165cbbd03
Andre Girao <andrelgirao22@hotmail.com>
2020-04-11 15:22:41 -0300

    criado projeto brewer

---
+++ src/main/java/com/alg/brewer/model/Cerveja.java
@@ -0,0 +1,36 @@
+package com.alg.brewer.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cerveja {
+
+	@NotBlank(message = "Sku é obrigatório")
+	private String sku;
+
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+
+	@Size(min = 1, max = 50)
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
